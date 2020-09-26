8fdbab53628fda13fca167dab183d5a48586d495
ogubuike245 <ogubuike245@users.noreply.github.com>
2020-09-26 13:19:10 +0300

    update

--- js/app.js
+++ js/app.js
@@ -16,23 +16,23 @@ particlesJS('particles',
   {
     "particles": {
       "number": {
-        "value": 30,
+        "value": 20,
         "density": {
           "enable": true,
-          "value_area": 400
+          "value_area": 800
         }
       },
       "color": {
-        "value": "#1C2FE3"
+        "value": "#177C85"
       },
       "shape": {
-        "type": "circle",
+        "type": "polygon",
         "stroke": {
-          "width": 5,
+          "width": 0,
           "color": "pink"
         },
         "polygon": {
-          "nb_sides": 9
+          "nb_sides": 6
         },
         "image": {
           "src": "v.jpg",
@@ -41,41 +41,41 @@ particlesJS('particles',
         }
       },
       "opacity": {
-        "value": 0.5,
-        "random": true,
+        "value": 1.5,
+        "random": false,
         "anim": {
           "enable": true,
-          "speed": 8,
-          "opacity_min": 0.1,
-          "sync": false
+          "speed": 1,
+          "opacity_min": 0.7,
+          "sync": true
         }
       },
       "size": {
-        "value": 5,
-        "random": true,
+        "value": 50,
+        "random": false,
         "anim": {
           "enable": true,
-          "speed": 40,
-          "size_min": 1.1,
-          "sync": true
+          "speed": 20,
+          "size_min": 60,
+          "sync": false
         }
       },
       "line_linked": {
         "enable": true,
-        "distance": 150,
-        "color": "#AA09DF",
-        "opacity": 0.4,
-        "width": 8
+        "distance": 800,
+        "color": "#ffffff",
+        "opacity": 1.4,
+        "width": 2
       },
       "move": {
         "enable": true,
-        "speed": 2,
-        "direction": "top",
+        "speed": 10,
+        "direction": "none",
         "random": false,
         "straight": false,
         "out_mode": "out",
         "attract": {
-          "enable": false,
+          "enable": true,
           "rotateX": 600,
           "rotateY": 1200
         }
@@ -85,24 +85,24 @@ particlesJS('particles',
       "detect_on": "canvas",
       "events": {
         "onhover": {
-          "enable": true,
+          "enable": false,
           "mode": "repulse"
         },
         "onclick": {
-          "enable": true,
+          "enable": false,
           "mode": "push"
         },
         "resize": true
       },
       "modes": {
-        "grab": {
+        "move": {
           "distance": 400,
           "line_linked": {
-            "opacity": 1
+            "opacity": 5
           }
         },
         "bubble": {
-          "distance": 400,
+          "distance": 4000,
           "size": 40,
           "duration": 2,
           "opacity": 8,
