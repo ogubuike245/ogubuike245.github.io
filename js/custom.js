0bc0d852301a18c272c62f76fdff9b62cf7fee54
ogubuike245 <ogubuike245@users.noreply.github.com>
2020-10-02 12:31:59 +0300

    Update custom.js

--- js/custom.js
+++ js/custom.js
@@ -1,36 +1,36 @@
 
 // ----- GOOGLE MAP ----- //
-var map = '';
-var center;
+// var map = '';
+// var center;
 
-function initialize() {
-    var mapOptions = {
-      zoom: 16,
-      center: new google.maps.LatLng(0.3059, 32.5910),
-      scrollwheel: true
-    };
+// function initialize() {
+//     var mapOptions = {
+//       zoom: 16,
+//       center: new google.maps.LatLng(0.3059, 32.5910),
+//       scrollwheel: true
+//     };
   
-    map = new google.maps.Map(document.getElementById('map-canvas'),  mapOptions);
+//     map = new google.maps.Map(document.getElementById('map-canvas'),  mapOptions);
 
-    google.maps.event.addDomListener(map, 'idle', function() {
-        calculateCenter();
-    });
+//     google.maps.event.addDomListener(map, 'idle', function() {
+//         calculateCenter();
+//     });
   
-    google.maps.event.addDomListener(window, 'resize', function() {
-        map.setCenter(center);
-    });
-}
+//     google.maps.event.addDomListener(window, 'resize', function() {
+//         map.setCenter(center);
+//     });
+// }
 
-function calculateCenter() {
-  center = map.getCenter();
-}
+// function calculateCenter() {
+//   center = map.getCenter();
+// }
 
-function loadGoogleMap(){
-    var script = document.createElement('script');
-    script.type = 'text/javascript';
-    script.src = 'https://maps.googleapis.com/maps/api/js?v=3.exp&sensor=false&' + 'callback=initialize';
-    document.body.appendChild(script);
-}
+// function loadGoogleMap(){
+//     var script = document.createElement('script');
+//     script.type = 'text/javascript';
+//     script.src = 'https://maps.googleapis.com/maps/api/js?v=3.exp&sensor=false&' + 'callback=initialize';
+//     document.body.appendChild(script);
+// }
 
 /* HTML document is loaded. DOM is ready. 
 -------------------------------------------*/
