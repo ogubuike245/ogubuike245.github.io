3f2709497bdb7faa37e492a3d9eef152d2f96745
ogubuike245 <ogubuike245@users.noreply.github.com>
2020-10-03 03:27:30 +0300

    SIDEBAR

--- js/custom.js
+++ js/custom.js
@@ -1,74 +1,95 @@
-
-// ----- GOOGLE MAP ----- //
-// var map = '';
-// var center;
-
-// function initialize() {
-//     var mapOptions = {
-//       zoom: 16,
-//       center: new google.maps.LatLng(0.3059, 32.5910),
-//       scrollwheel: true
-//     };
-  
-//     map = new google.maps.Map(document.getElementById('map-canvas'),  mapOptions);
-
-//     google.maps.event.addDomListener(map, 'idle', function() {
-//         calculateCenter();
-//     });
-  
-//     google.maps.event.addDomListener(window, 'resize', function() {
-//         map.setCenter(center);
-//     });
-// }
-
-// function calculateCenter() {
-//   center = map.getCenter();
-// }
-
-// function loadGoogleMap(){
-//     var script = document.createElement('script');
-//     script.type = 'text/javascript';
-//     script.src = 'https://maps.googleapis.com/maps/api/js?v=3.exp&sensor=false&' + 'callback=initialize';
-//     document.body.appendChild(script);
-// }
-
-/* HTML document is loaded. DOM is ready. 
+/* HTML document is loaded. DOM is ready.
 -------------------------------------------*/
 $(function(){
-
-   // --------- HIDE MOBILE MENU AFTER CLIKING ON A LINK ------- //
-    $('.navbar-collapse a').click(function(){
-        $(".navbar-collapse").collapse('hide');
-    });
-
-  // --------- PORTFOLIO IMAGE ----- //
-  $('#portfolio a').nivoLightbox({
-        effect: 'fadeScale',
-    });
-
-
-
-  // ------- WOW ANIMATED ------ //
-  wow = new WOW(
-  {
-    mobile: false
-  });
-  wow.init();
-
-  // ------- GOOGLE MAP ----- //
-  loadGoogleMap();
-
-  // ------- JQUERY PARALLAX ---- //
-  function initParallax() {
-    // $('#home').parallax("100%", 1.3);
-    $('#team').parallax("100%", 3.3);
-    $('#contact').parallax("100%", 2.1);
-
-  }
-  initParallax();
-
-
-
+// --------- HIDE MOBILE MENU AFTER CLIKING ON A LINK ------- //
+$('.navbar-collapse a').click(function(){
+$(".navbar-collapse").collapse('hide');
+});
+// --------- PORTFOLIO IMAGE ----- //
+$('#portfolio a').nivoLightbox({
+effect: 'fadeScale',
+});
+// ------- WOW ANIMATED ------ //
+wow = new WOW(
+{
+mobile: false
+});
+wow.init();
+// ------- GOOGLE MAP ----- //
+loadGoogleMap();
+// ------- JQUERY PARALLAX ---- //
+function initParallax() {
+// $('#home').parallax("100%", 1.3);
+$('#team').parallax("100%", 3.3);
+$('#contact').parallax("100%", 2.1);
+}
+initParallax();
 });
 
+
+(function($) {
+
+  var $window = $(window),
+    $body = $('body'),
+    $menu = $('#menu'),
+    $toggle = $('.toggle'),
+    
+    $sidebar = $('#sidebar'),
+    $main = $('#main');
+
+  // Breakpoints.
+    breakpoints({
+      xlarge:   [ '1281px',  '1680px' ],
+      large:    [ '981px',   '1280px' ],
+      medium:   [ '737px',   '980px'  ],
+      small:    [ '481px',   '736px'  ],
+      xsmall:   [ null,      '480px'  ]
+    });
+
+  
+
+  // Menu.
+    $menu
+      .appendTo($body)
+      .panel({
+        delay: 500,
+        hideOnClick: true,
+        hideOnSwipe: true,
+        resetScroll: true,
+        resetForms: true,
+        side: 'right',
+        target: $body,
+        visibleClass: 'is-menu-visible'
+      });
+
+     
+      
+
  
+
+  // Intro.
+    var $intro = $('#intro');
+
+    // Move to main on <=large, back to sidebar on >large.
+      breakpoints.on('<=large', function() {
+        $intro.prependTo($main);
+      });
+
+      breakpoints.on('>large', function() {
+        $intro.prependTo($sidebar);
+      });
+
+})(jQuery);
+
+
+
+// $(document).ready(function() {
+//   $('.toggle').click(function(){
+//     $('.toggle').toggleClass('active')
+    
+
+//       });
+
+  
+// });
+
